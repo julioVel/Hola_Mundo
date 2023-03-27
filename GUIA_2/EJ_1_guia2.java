b3cbbc3d2a4392a82e29dc7ac6c766bdcc7570aa
julioVel <127145608+julioVel@users.noreply.github.com>
2023-03-26 21:15:53 -0300

    Update EJ_1_guia2.java

--- GUIA_2/EJ_1_guia2.java
+++ GUIA_2/EJ_1_guia2.java
@@ -6,11 +6,25 @@ package com.mycompany.egg_guia2;
 
 /**
  *
- * @author lautaro
+ * @author lautar
  */
 public class EGG_Guia2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1;
+        int num2;
+        int suma = 0;
+       
+       Scanner numeros= new Scanner(System.in);
+       
+       System.out.println("ingrese el primer numero");
+       num1 = numeros.nextInt();
+       
+       System.out.println("ingrese el segundo numero");
+       num2 = numeros.nextInt();
+       
+       suma = num1 + num2;
+       
+       System.out.println("la suma de: "+num1+"+"+num2+"="+suma);
     }
 }
