44d7807a2b11276e38eed0530044adc92cce2e8c
julioVel <127145608+julioVel@users.noreply.github.com>
2023-03-26 21:19:11 -0300

    Update EJ_3_guia2.java

--- GUIA_2/EJ_3_guia2.java
+++ GUIA_2/EJ_3_guia2.java
@@ -12,9 +12,23 @@ public class EJ_3_guia2 {
 
     /**
      * @param args the command line arguments
+     * Escribir un programa que pida una frase 
+     * y la muestre toda en mayúsculas 
+     * y después toda en minúsculas.
+     * investigar la función toUpperCase() 
+     * y toLowerCase() en Java.
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       String frase;
+      
+      Scanner strFrase = new Scanner(System.in);
+      
+      System.out.println("ingrese una frase");
+      frase = strFrase.nextLine();
+      
+      System.out.println("frase a mayusculas--->"+frase.toUpperCase());
+      System.out.println("frase a minusculas--->"+frase.toLowerCase());
     }
     
 }
