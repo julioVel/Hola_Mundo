7cd5fddfdd7f5198c212a41da771804a0cb3e1fb
julioVel <127145608+julioVel@users.noreply.github.com>
2023-03-26 21:20:25 -0300

    Update EJ_4_guia2.java

--- GUIA_2/EJ_4_guia2.java
+++ GUIA_2/EJ_4_guia2.java
@@ -12,9 +12,25 @@ public class EJ_4_guia2 {
 
     /**
      * @param args the command line arguments
+     * Dada una cantidad de grados centígrados
+     * se debe mostrar su equivalente en grados Fahrenheit.
+     * La fórmula correspondiente es:
+     * F = 32 + (9 * C / 5).
+     * (13 °C × 9/5) + 32 =
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       double gCentigrados;
+       double Fahrenheit;
+       
+       Scanner strCentigrados = new Scanner(System.in);
+       
+       System.out.println("ingrese grados centigrados:");
+       gCentigrados = strCentigrados.nextInt();
+       
+       Fahrenheit = (gCentigrados * 9/5) + 32;
+       
+       System.out.print("su equivalente en grados Fahrenheit es:"+Fahrenheit);
     }
     
 }
